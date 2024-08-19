48e45740fa7a259987acbb0d8f0498a99bbc83f2
Nedim Zečić <nedim@rebb.it>
2024-08-20 02:20:16 +0400

    initial commit

---
+++ msl/helper.swift
@@ -0,0 +1,63 @@
+import Foundation
+import Virtualization
+
+class Delegate: NSObject {
+}
+
+extension Delegate: VZVirtualMachineDelegate {
+  func guestDidStop(_ virtualMachine: VZVirtualMachine) {
+    print("The guest shut down. Exiting.")
+    exit(EXIT_SUCCESS)
+  }
+}
+
+func createBootLoader(kernelURL: URL, ramdiskURL: URL, cmd: String) -> VZBootLoader {
+  let bootLoader = VZLinuxBootLoader(kernelURL: kernelURL)
+  bootLoader.initialRamdiskURL = ramdiskURL
+  bootLoader.commandLine = cmd
+
+  return bootLoader
+}
+
+func createConsoleConfiguration() -> VZSerialPortConfiguration {
+  let consoleConfiguration = VZVirtioConsoleDeviceSerialPortConfiguration()
+
+  let inputFileHandle = FileHandle.standardInput
+  let outputFileHandle = FileHandle.standardOutput
+
+  // Put stdin into raw mode, disabling local echo, input canonicalization,
+  // and CR-NL mapping.
+  var attributes = termios()
+  tcgetattr(inputFileHandle.fileDescriptor, &attributes)
+  //attributes.c_iflag &= ~tcflag_t(ICRNL)
+  //attributes.c_lflag &= ~tcflag_t(ICANON | ECHO)
+  tcsetattr(inputFileHandle.fileDescriptor, TCSANOW, &attributes)
+
+  let stdioAttachment = VZFileHandleSerialPortAttachment(fileHandleForReading: inputFileHandle, fileHandleForWriting: outputFileHandle)
+
+  consoleConfiguration.attachment = stdioAttachment
+
+  return consoleConfiguration
+}
+
+func createStorageDeviceConfiguration(disk: String?, nvme: Bool) throws -> VZStorageDeviceConfiguration {
+  let diskImageURL = URL(fileURLWithPath: disk!, isDirectory: false)
+  let diskImageAttachment = try VZDiskImageStorageDeviceAttachment(url: diskImageURL, readOnly: false)
+
+  if #available(macOS 14.0, *), nvme {
+    return VZNVMExpressControllerDeviceConfiguration(attachment: diskImageAttachment)
+  }
+
+  return VZVirtioBlockDeviceConfiguration(attachment: diskImageAttachment)
+}
+
+func createNetworkDeviceConfiguration(mac: String?) -> VZNetworkDeviceConfiguration {
+  let networkDevice = VZVirtioNetworkDeviceConfiguration()
+  networkDevice.attachment = VZNATNetworkDeviceAttachment()
+
+  if (mac != nil) {
+    networkDevice.macAddress = VZMACAddress(string: mac!) ?? VZMACAddress.randomLocallyAdministered()
+  }
+  
+  return networkDevice
+}
---
+++ msl/main.swift
@@ -0,0 +1,69 @@
+import Foundation
+import Virtualization
+import ArgumentParser
+
+struct MSL: ParsableCommand {
+  @Option(help: "Number of CPUs VM will use")
+  var cpu: UInt8 = 1
+
+  @Option(help: "RAM size in MB")
+  var ram: UInt64 = 1024
+
+  @Option(help: "Kernel image path")
+  var kernel: String = "vmlinuz"
+
+  @Option(help: "Ramdisk image path")
+  var ramdisk: String = "initrd"
+
+  @Option(help: "Root image path")
+  var disk: String?
+
+  @Option(help: "Kernel's command-line parameters")
+  var cmd: String = "console=hvc0 rd.break=initqueue"
+
+  @Option(help: "Network device MAC address")
+  var mac: String?
+
+  @Flag(help: "Create storage as NVME storage device")
+  var nvme = false
+
+  func run() throws {
+    print(disk!)
+    let kernelURL = URL(fileURLWithPath: kernel, isDirectory: false)
+    let ramdiskURL = URL(fileURLWithPath: ramdisk, isDirectory: false)
+
+    let configuration = VZVirtualMachineConfiguration()
+    configuration.cpuCount = Int(cpu)
+    configuration.memorySize = ram * 1048576
+    configuration.entropyDevices = [VZVirtioEntropyDeviceConfiguration()]
+    configuration.serialPorts = [ createConsoleConfiguration() ]
+    configuration.bootLoader = createBootLoader(kernelURL: kernelURL, ramdiskURL: ramdiskURL, cmd: cmd)
+
+    if (disk != nil) {
+      configuration.storageDevices = [ try createStorageDeviceConfiguration(disk: disk, nvme: nvme) ]
+    }
+
+    configuration.networkDevices = [ createNetworkDeviceConfiguration(mac: mac) ]
+
+    do {
+      try configuration.validate()
+    } catch {
+      MSL.exit(withError: error)
+    }
+
+    let virtualMachine = VZVirtualMachine(configuration: configuration)
+
+    let delegate = Delegate()
+    virtualMachine.delegate = delegate
+
+    virtualMachine.start { (result) in
+      if case let .failure(error) = result {
+        MSL.exit(withError: error)
+      }
+    }
+
+    RunLoop.main.run(until: Date.distantFuture)
+  }
+}
+
+MSL.main()
